b26817ddabcb26eabca60c32cb6d2d9051e66170
Alejandro Manuel Seva Arroyo <asevaa@fp.uoc.edu>
2019-05-25 19:52:28 +0200

    Test eliminar cuenta
    Se añade el mensaje de confirmación también al botón de elminar cuenta.

--- view/productoView.php
+++ view/productoView.php
@@ -22,7 +22,26 @@
        <div class="col-lg-2 offset-lg-3">
            <a href="<?php echo $helper->url("usuario","cerrar"); ?>" class="btn btn-light">Cerrar Sesión</a></div>
        <div class="col-lg-2 offset-lg-3">
-           <a href="<?php echo $helper->url("usuario","eliminar"); ?>&id=<?php echo $_SESSION["IdId"]; ?>&apodo=<?php echo $_SESSION["IdUsuario"] ?>&perfil=<?php echo $_SESSION["IdPerfil"]; ?>" class="btn btn-danger">Eliminar cuenta</a>
+           <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#eliminarCuenta">
+                    Eliminar cuenta
+                    </button>
+                     <div class="modal text-danger" id="eliminarCuenta">
+                      <div class="modal-dialog">
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <h4 class="modal-title">Eliminar cuenta</h4>
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          </div>
+                          <div class="modal-body">
+                            ¡Cuidado! Si continúa eliminará la cuenta de manera definitiva y no podrá acceder más con ella. 
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                            <a href="<?php echo $helper->url("usuario","eliminar"); ?>&id=<?php echo $_SESSION["IdId"]; ?>&apodo=<?php echo $_SESSION["IdUsuario"] ?>&perfil=<?php echo $_SESSION["IdPerfil"]; ?>" class="btn btn-danger">Continuar</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
        </div>
        </div>
    <hr />
